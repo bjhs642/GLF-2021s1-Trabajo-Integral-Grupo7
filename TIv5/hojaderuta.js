1002c1985a072d04c40b4f4a93f395a00facdbe9
ReenaPalominos <82245870+ReenaPalominos@users.noreply.github.com>
2021-07-15 23:33:55 -0400

    Add files via upload
    Arreglo de bugs

---
+++ TIv5/hojaderuta.js
@@ -0,0 +1,18 @@
+export function calculohoja(auxcentro,auxpunto){
+    var puntoscentro=auxcentro;
+    var puntosventa=auxpunto;
+    console.log('Entro a la funcion');
+    console.log(puntoscentro);
+    console.log(puntosventa);
+    /*Codigo de ejemplo
+    El usuario quiere el recorrido del camion
+    Del centro 1
+    A los puntos de venta 2 4 5
+    */
+   var ar=[1,2,4,5];//El centro 1 es la posicion 0 de puntos de centros
+
+
+
+
+
+}
